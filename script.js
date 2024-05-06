5259125f9cf7ca1eee0fedcbe05b3af2e7bd6122
Nema <kingshousecn@gmail.com>
2024-05-06 11:24:56 +0100

    Adding comments to JS for git commands

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//  git status                 -- checks status of saved files
+//  git add .                  -- add all files into ready for commit 
+//  git commit -m "COMMENT"    -- creates a commit with a comment
+//  git push -u origin master  -- commits to online Repo (MASTER)
+
 const array1 = [' Cat ', ' Dog ', ' Mouse '];
 const diction1 = [{}];
 
